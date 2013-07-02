fd2a70c97d66616ec37673d656a26532cd883d33
Karin Lammering <karin@rowland.nl>
2013-07-02 14:04:46 +0200

    Making the contact page

--- contact.php
+++ contact.php
@@ -1,7 +1,23 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<?php include("includes/header.php"); ?>
+<div class="content-header">
+    <h1>Contact</h1>
+</div>
+<div class="content cf">
+    <p>vul het onderstaande formulier in en wij nemen zo spoedig mogelijk contact met u op</p>
+    <div class="naam">
+        <label>Naam:</label>
+        <input type="text" id="Name" name="Name" />
+    </div>
+    <div class="email">
+        <label>Email adres:</label>
+        <input type="email" id="email" name="email" />
+    </div>
+    <div class="vraag">
+        <label class="labelvraag">Uw vraag aan ons:</label>
+        <textarea name="textarea" id="textarea" rows="20" cols="60">Stel hier uw vraag aan ons</textarea>
+    </div>
+    <div class="submit">
+        <input type="submit" id="submit" name="submit" value="verzenden" />
+    </div>
+</div>
+    <?php include("includes/footer.php"); ?>
