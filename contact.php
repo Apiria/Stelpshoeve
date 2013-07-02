950ac1f8ed0b43c4ee1808988eae445a273840d4
Karin Lammering <karin@rowland.nl>
2013-07-02 14:10:09 +0200

    fixes minor grammar issues

--- contact.php
+++ contact.php
@@ -4,16 +4,16 @@
 </div>
 <div class="content cf">
     <p>vul het onderstaande formulier in en wij nemen zo spoedig mogelijk contact met u op</p>
-    <div class="naam">
+    <div class="name">
         <label>Naam:</label>
-        <input type="text" id="Name" name="Name" />
+        <input type="text" id="name" name="name" />
     </div>
     <div class="email">
         <label>Email adres:</label>
         <input type="email" id="email" name="email" />
     </div>
-    <div class="vraag">
-        <label class="labelvraag">Uw vraag aan ons:</label>
+    <div class="question">
+        <label class="labelquestion">Uw vraag aan ons:</label>
         <textarea name="textarea" id="textarea" rows="20" cols="60">Stel hier uw vraag aan ons</textarea>
     </div>
     <div class="submit">
